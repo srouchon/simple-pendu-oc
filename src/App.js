0c65ee324d95763f29b2d3d84197f1ef1e980f17
sophie rouchon <sophie.rouchon@gmail.com>
2020-11-30 08:39:50 +0100

    remove comments

--- src/App.js
+++ src/App.js
@@ -35,10 +35,3 @@ class App extends Component {
 }
 
 export default App;
-
-// Produit une représentation textuelle de l’état de la partie,
-// chaque lettre non découverte étant représentée par un _underscore_.
-// (CSS assurera de l’espacement entre les lettres pour mieux visualiser le tout).
-// function computeDisplay(phrase, usedLetters) {
-  // return phrase.replace(/\w/g,
-    // (letter) => (usedLetters.has(letter) ? letter : '_')  )}...
